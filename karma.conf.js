f1fbc1bb15e54be5e04e56d183489bf9c1ee4e12
Mark Topper <mail@webman.io>
2015-09-09 21:41:10 +0200

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function(config) {
     files: [
         'node_modules/angular/angular.js',
         'node_modules/angular-mocks/angular-mocks.js',
-        'src/ngTouch.js',
+        'src/ngTouchstart.js',
         'tests/**/*.js',
         'tests/*.js'
     ],
